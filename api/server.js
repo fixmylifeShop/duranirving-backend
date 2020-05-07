ab28ace4e3f75f667de9fccfbf30514807164555
fixmylifedesigns <ijd.irving@gmail.com>
2020-05-07 13:47:38 -0400

    cors

--- api/server.js
+++ api/server.js
@@ -8,14 +8,14 @@ const cookieParser = require("cookie-parser");
 var whitelist = process.env.WHITELIST_CORS;
 
 var corsOptions = {
-  origin: //"*",
-  function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
+  // function (origin, callback) {
+  //   if (whitelist.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"));
+  //   }
+  // },
   credentials: true,
 };
 
