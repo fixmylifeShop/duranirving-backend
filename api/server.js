08f8b95d3fddd6b718a331be8854e434b0c07737
fixmylifedesigns <ijd.irving@gmail.com>
2020-05-13 18:22:55 -0400

    comment out passport

--- api/server.js
+++ api/server.js
@@ -32,19 +32,19 @@ server.use(cors());
 
 
 // const passportRouter = require("../router/auth/passportRouter")
-// const authRouter = require("../router/auth/authRouter");
-// const mailerRouter = require("../router/mailer/mailerRouter");
-// const usersRouter = require("../router/users/usersRouter");
-// const shopsRouter = require("../router/store/shops/shopsRouter");
-// const shopviewsRouter = require("../router/store/shopViews/shopViewsRouter");
-// const cart = require("../router/store/cart/cartRouter");
-// server.use("/api/", authRouter);
+const authRouter = require("../router/auth/authRouter");
+const mailerRouter = require("../router/mailer/mailerRouter");
+const usersRouter = require("../router/users/usersRouter");
+const shopsRouter = require("../router/store/shops/shopsRouter");
+const shopviewsRouter = require("../router/store/shopViews/shopViewsRouter");
+const cart = require("../router/store/cart/cartRouter");
+server.use("/api/", authRouter);
 // server.use("/api/auth/", passportRouter);
-// server.use("/api/mailer", mailerRouter);
-// server.use("/api/users", usersRouter);
-// server.use("/api/shops", shopsRouter);
-// server.use("/api/views", shopviewsRouter);
-// server.use("/api/cart", cart);
+server.use("/api/mailer", mailerRouter);
+server.use("/api/users", usersRouter);
+server.use("/api/shops", shopsRouter);
+server.use("/api/views", shopviewsRouter);
+server.use("/api/cart", cart);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "DuranIrving api running" });
